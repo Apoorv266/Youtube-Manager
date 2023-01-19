ceae0ae531a2cc110be188b3457bd16ad2208d6d
Apoorv Sharma <apoorvsharma494@gmail.com>
2023-01-19 13:02:22 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename='/Youtube-Manager'>
+  <BrowserRouter>
   <Auth0Provider
     domain="dev-8tlt1ay2nvpy7i3f.us.auth0.com"
     clientId="CYBWuf4MYPlPtsZ4irVdmLK1JteBuU3D"
-    redirectUri="http://localhost:3000/collection"
+    redirectUri="https://youtubebuddy.netlify.app/collection"
   >
 
     <App />
