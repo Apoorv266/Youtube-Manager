e146191ea4c591eab77dfa2ee756f5d46bb42de6
Apoorv Sharma <apoorvsharma494@gmail.com>
2023-01-19 10:41:19 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename='/Youtube-Manager'>
   <Auth0Provider
     domain="dev-8tlt1ay2nvpy7i3f.us.auth0.com"
     clientId="CYBWuf4MYPlPtsZ4irVdmLK1JteBuU3D"
